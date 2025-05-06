696c3a3fefd2eaffb7c5101504b17d04de2ed459
hahahellooo <hahahello777@gmail.com>
2025-05-06 13:30:53 +0900

    2-14 웹페이지 디자인하기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -7,19 +7,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 // import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 import lombok.RequiredArgsConstructor;
 
+// prefix를 '/question'으로 고정
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     
-    private final QuestionRepository questionRepository;
+    // private final QuestionRepository questionRepository;
 
-    @GetMapping("/question/list")
+    // Repository 대신 Service로 연결
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
+        // List<Question> questionList = this.questionRepository.findAll();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    // 질문 상세 페이지 URL 매핑
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        // return "question_detail";
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
     // question_list.html과 웹페이지 연결
     // @GetMapping("/question/list")
